3400086fe2cb611d3036f4397d84df8b3ab862b0
S K Sarfaraz <sarfaraz7856sk@gmail.com>
2025-07-06 02:12:20 +0530

    9th tryh commit

--- app/admin/_components/AddBlog.tsx
+++ app/admin/_components/AddBlog.tsx
@@ -8,9 +8,6 @@ import { useId } from "react";
 import { toast } from "sonner";
 
 
-// Dynamically import TinyMCE Editor
-import dynamic from "next/dynamic";
-
 const Editor = dynamic(
   () => import("@tinymce/tinymce-react").then((mod) => mod.Editor as any),
   {
